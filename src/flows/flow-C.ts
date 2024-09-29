0d4e75d4de7f5a580d4d6312a142616a315a19b1
RDeveloperTrial <adevelopertestingdevice@gmail.com>
2024-09-29 22:55:44 +0200

    formatted wa-text

--- src/flows/flow-C.ts
+++ src/flows/flow-C.ts
@@ -4,7 +4,15 @@ import { getEmails } from "../utils/gmailServices.js"
 const flowEmails = addKeyword('LeermailC')
   .addAction( async (_, { flowDynamic }) => {
     const emailObjects = await getEmails(2)
-    const emailJSONs = emailObjects.map(item => ({body:`From: ${item.from} \nSubject: ${item.subject} \nSnippet: ${item.snippet}`}))
+    const emailJSONs = emailObjects.map(item => 
+      ({body:`
+    📧 *${item.from}*
+    -----------------------------
+    *Asunto*: ${item.subject}
+    -----------------------------
+    *Resumen*: ${item.snippet}
+
+    `}))   
     await flowDynamic(emailJSONs)
   })
 
--- src/flows/flow-R.ts
+++ src/flows/flow-R.ts
@@ -4,7 +4,15 @@ import { getEmails } from "../utils/gmailServices.js"
 const flowEmails = addKeyword('LeermailR')
 .addAction( async (_, { flowDynamic }) => {
     const emailObjects = await getEmails(1)
-    const emailJSONs = emailObjects.map(item => ({body:`From: ${item.from} \nSubject: ${item.subject} \nSnippet: ${item.snippet}`}))
+    const emailJSONs = emailObjects.map(item => 
+      ({body:`
+    📧 *${item.from}*
+    -----------------------------
+    *Asunto*: ${item.subject}
+    -----------------------------
+    *Resumen*: ${item.snippet}
+
+    `}))
     await flowDynamic(emailJSONs)
   })
 
